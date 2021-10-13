3762a4d31a4242226402ba8ca4bda2f41367583a
kledenai <bruno.silva1643@etec.sp.gov.br>
2021-10-13 01:30:13 -0300

    update author name

--- radicale_auth_ldap/setup.py
+++ radicale_auth_ldap/setup.py
@@ -6,7 +6,7 @@ setup(
     name="radicale-auth-ldap",
     version="0.1",
     description="LDAP Authentication Plugin for Radicale 2",
-    author="Raoul Thill",
+    author="Kledenai Ashver",
     license="GNU GPL v3",
     install_requires=["radicale >= 2.0", "ldap3 >= 2.3, < 2.4"],
     packages=["radicale_auth_ldap"])
